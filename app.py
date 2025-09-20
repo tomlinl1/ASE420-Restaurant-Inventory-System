e45a4a0693ac6775cb453e6026f800cfa86b24e8
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2025-09-20 10:41:46 -0400

    added more routes and template pages for content to be placed

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return "Welcome to Antonio's Pizza Pub Inventory System."
+    return render_template("index.html")
+
+@app.route("/view")
+
+def viewStock():
+    return render_template("view-stock.html")
+
+@app.route("/create")
+
+def createStock():
+    return render_template("create-stock.html")
 
 @app.route("/add", methods=["POST"])
 def add():
     return "This is how you will add your items"
+
+@app.route("/delete", methods={"POST"})
+def delete():
+    return "This is where stuff will be deleted"
+
+@app.route("/update", methods=["POST"])
+def update():
+    return "This is where things are updated"
+
 
 
 
